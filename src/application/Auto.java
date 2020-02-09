a4a8ff6b10b26e0944f41bfaaad0f1669758ba65
GitGudLmao <spamgamer93@gmail.com>
2020-02-09 11:28:26 +0100

    Grobe outline und attribute der Autoclass

--- src/application/Auto.java
+++ src/application/Auto.java
@@ -1,5 +1,36 @@
 package application;
 
 public class Auto {
+	private String name;
+	private String farbe;
+	private int speed;
+	private int haftung;
+	private int handycap;
+	
+	
+	public Auto(String name, String farbe, int speed, int haftung, int handycap) {
+		this.name=name;
+		this.farbe = farbe;
+		this.speed = speed;
+		this.haftung=haftung;
+		this.handycap=handycap;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getFarbe() {
+		return farbe;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public int getHaftung() {
+		return haftung;
+	}
+	public int getHandycap() {
+		return handycap;
+	}
+	
+	
 
 }
